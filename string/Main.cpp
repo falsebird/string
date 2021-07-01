484291f79f2c6117cb517c649c4342a095b1608d
luoziyuan <874105638@qq.com>
2021-07-01 16:29:27 +0800

    add push_back pop_back end begin  clear empty at function

--- string/Main.cpp
+++ string/Main.cpp
@@ -1,6 +1,10 @@
 #include<iostream>
+#include<iterator>
 #include "myString.h"
+#include "String_T.h"
 using namespace std;
+
+
 
 int testgouzao() {
     String st = "hello";
@@ -22,7 +26,26 @@ int test_copy() {
    
 }
 
+void test_cout()
+{
+    String_T<int> a(5);
+    a.push_back(1);//—È÷§push_back
+    a.push_back(2);
+    a.push_back(3);
+    cout << a << endl;
+    int *ita = a.begin();
+
+    cout << *ita << endl;
+    cout << *++ita << endl;
+    int *endit = a.end();
+    ita = ++ita;
+    if (ita == endit) {
+        cout << "end is here" << endl;
+    }
+    
+}
+
 int main() {
-    test_copy();
+    test_cout();
     return 0;
 }
--- string/String_T.h
+++ string/String_T.h
@@ -6,8 +6,16 @@ using namespace std;
 template <class T>
 class String_T {
 	//重载输入输出
-	friend ostream& operator<<(ostream& out, const String_T<T>& st);
-	friend istream& operator>>(istream& in, const String_T& st);
+	friend ostream& operator<<(ostream& out, const String_T<T>& st)
+	{
+
+		for (int i = 0; i < st._size; i++)
+		{
+			out << st._data[i];
+		}
+		return out;
+	}
+	//friend istream& operator>>(istream& in, const String_T& st);
 public:
 	T* _data;
 	size_t _capacity;
@@ -24,9 +32,9 @@ public:
 	}
 
 	//初始化容积
-	String_T<T>(size_t size) {
-		this->_size = size;
-		this->_capacity = size;
+	String_T<T>(size_t capacity) {
+		this->_size = 0;
+		this->_capacity = capacity;
 		_data = new T[_capacity];
 	}
 
@@ -48,15 +56,21 @@ public:
 	}
 
 	//基础功能
+
+	//容积操作
 	size_t size() const { return _size; };
 	size_t capacity() const { return _capacity; };
+
+	//迭代器
+	iterator begin() { return &_data[0]; };
+	iterator end() { return &_data[this->_size - 1]; };
 	void clear() {
 		//删除内容
 		if (_data){
 			delete[] _data;
 			this->_size = 0;
 			this->_capacity = 0;
-			this->_data[0] = '\0'
+			this->_data = nullptr;
 		}
 		
 	}
@@ -64,51 +78,66 @@ public:
 		//
 		return this->_size == 0;
 	}
-	T& at(size_t index);
+	T& at(const size_t &index) {
+		if (empty() || index > _capacity)
+			return (T)NULL;
+		return _data[index];
+	}
 
-	//尾插 尾删
-	void push_back(const T &val) {
-		int new_capacity = this->_size + val->_size;
-		if (new_capacity > this-> _capacity)
+
+	//尾插 
+	void push_back(const T val) {
+		int new_capacity = this->_size + 1;
+		//空间不够扩展空间
+		if (new_capacity > this->_capacity)
 		{
-			
-			if (new_capacity > _capacity)
+			//开空间
+			T* tmp = new T[2 * this->_capacity];
+			//拷贝
+			for (int i = 0; i < _size; i++)
 			{
-				//开空间
-				char* tmp = new char[new_capacity + 1];
-				//拷贝
-				for (int i = 0; i < _size; i++)
-				{
-					tmp[i] = _str[i];
-				}
-				//释放原有空间
-				delete[] _str;
-				//指向新的空间
-				_str = tmp;
-				//更新
-				_capacity = new_capacity;
-				_size = new_capacity;
+				tmp[i] = this->_data[i];
+			}
+			//释放原有空间
+			if (this->_data)
+				delete[] this->_data;
+			//指向新的空间
+			this->_data = tmp;
+			//更新
+		}
+			
+
+		this->_data[new_capacity - 1] = val;
+		this->_capacity = 2 * this->_capacity;
+		this->_size = new_capacity;
+
+	}
+	// 尾删
+	T pop_back() {
+		if (!empty()) {
+			int nsize = this->_size;
+			T v = this->_data[nsize - 1];
+			this->_data[nsize - 1] = NULL;
+			this->_size = nsize - 1;
+			return v;
+		}
+		else {
+			return NULL;
+		}
+	}
+
 	
 
-			}
-			
-		}
-		strcpy(val + _size, val);
-		_size += strlen(str);
-	}
-	void pop_back();
-
-	//迭代器
-	iterator begin();
-	iterator end();
-
 	//删除插入
-	T* erase(T* pos);//删除指定元素
+	//删除指定元素
+	T* erase(T* pos) {
+		if (pos > begin()&&pos <end()){
+		}
+	}
 	T* insert(T* pos, T val);//指定位置插入元素
 
-	//容积操作
-	size_t size()const;
-	size_t capacity()const;
+	
+
 
 	//赋值
 	String_T<T>& operator=(const String_T<T>& obj) {
